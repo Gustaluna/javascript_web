15319312c006e2280c5dec53f41f89852292b030
Gustaluna <lgsantana.cin@gmail.com>
2023-01-23 17:16:51 -0300

    ?

--- js/principal.js
+++ js/principal.js
@@ -14,13 +14,13 @@ let tdImc = paciente.querySelector(".info-imc");
 let pesoValido = true;
 let alturaValida = true;
 
-if (peso < 0 || peso > 1000){
+if (peso <= 0 || peso >= 1000){
     console.log("Peso inválido")
     pesoValido = false;
     tdImc.textContent = "Peso inválido!"
 }
 
-if (altura < 0 || peso > 3){
+if (altura <= 0 || peso >= 3){
     console.log("Altura inválida")
     alturaValida = false;
     tdImc.textContent = "Altura inválida!"

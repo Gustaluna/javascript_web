171f64cfd92fdca31d9a72d9bd7325c799d85d75
Gustaluna <lgsantana.cin@gmail.com>
2023-01-22 23:01:28 -0300

    calculo IMC DOM

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,16 @@
 let titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente")
+var tdPeso = paciente.querySelector(".info-peso")
+
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura")
+var altura = tdAltura.textContent
+
+var tdImc = paciente.querySelector(".info-imc")
+
+var imc = peso / (altura * altura);
+
+tdImc.textContent = imc 

e457f0881c86a3f769446cc964aa785a9d056eeb
Gustaluna <lgsantana.cin@gmail.com>
2023-01-25 19:26:35 -0300

    style

--- js/principal.js
+++ js/principal.js
@@ -19,15 +19,17 @@ for (let i = 0; i < pacientes.length; i++){
     let alturaValida = true;
 
     if (peso <= 0 || peso >= 1000){
-        console.log("Peso inválido")
+        console.log("Peso inválido");
         pesoValido = false;
-        tdImc.textContent = "Peso inválido!"
+        tdImc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido");
     }
 
     if (altura <= 0 || altura >= 3.00){
-        console.log("Altura inválida")
+        console.log("Altura inválida");
         alturaValida = false;
-        tdImc.textContent = "Altura inválida!"
+        tdImc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido");
     }
 
     if (alturaValida && pesoValido){

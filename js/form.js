59f6f19f9fc384635969ca6dfb9941dcacbb4197
Gustaluna <lgsantana.cin@gmail.com>
2023-01-28 00:07:47 -0300

    Boas práticas

---
+++ js/form.js
@@ -0,0 +1,35 @@
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", (event) => {
+    event.preventDefault;
+
+    let form = document.querySelector("#form-adiciona");
+
+    let nome = form.nome.value;
+    let peso = form.peso.value;
+    let altura = form.altura.value;
+    let gordura = form.gordura.value;
+
+    let pacienteTr = document.createElement("tr");
+
+    let nomeTd = document.createElement("td")
+    let pesoTd = document.createElement("td")
+    let alturaTd = document.createElement("td")
+    let gorduraTd = document.createElement("td")
+    let imcTd = document.createElement("td")
+
+    nomeTd.textContent = nome
+    pesoTd.textContent = peso
+    alturaTd.textContent = altura
+    gorduraTd.textContent = gordura
+    imcTd.textContent = imc
+
+    pacienteTr.appendChild(nomeTd)
+    pacienteTr.appendChild(pesoTd)
+    pacienteTr.appendChild(alturaTd)
+    pacienteTr.appendChild(gorduraTd)
+
+    let tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr)
+})
